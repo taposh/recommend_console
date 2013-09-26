38bf605d7f0627a9ee4da6d59caf4dd145ca1141
Taposh Dutta Roy <taposh_dr@hotmail.com>
2013-09-26 13:54:31 -0700

    Added Advanced Formula

--- src/NonPersRecommender.java
+++ src/NonPersRecommender.java
@@ -1,45 +1,46 @@
 /**
- * Author: Taposh
+ * Program: Non-Personalized Recommendation
+ * Author: Taposh Dutta ROy
  * Date: 9/22/13
  * Time: 10:28 PM
  */
 import java.io.*;
-import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.math.RoundingMode;
 import java.util.*;
-
 import au.com.bytecode.opencsv.CSVReader;
 
 
 
-/*
-Your inputs for Programming Assignment 1 are:
-98
-854
-1637
- */
+/*Associations used - 98,854,1637*/
 
 class NonPersRecommender {
+
+    //top 5
+    public static final int TOP_5 = 5;
+    //path of document
+    public static final String DOC_PATH = "/Users/taposhdr/Dropbox/WebIntelligence/Reco_system/module2/recsys-data-ratings.csv";
+
+
 
 
     public static void main(String[] args) {
 
         String outputResult = "";
 
+        //Dataset for associate - Inputs
         ArrayList<Integer> Input = new ArrayList<Integer>();
-
         Input.add(98);
         Input.add(854);
         Input.add(1637);
 
-        String strFilePath = "/Users/taposhdr/Dropbox/WebIntelligence/Reco_system/module2/recsys-data-ratings.csv";
+        //File path
+        String strFilePath = DOC_PATH;
+
         try {
             int ij =0;
             for (ij=0; ij<Input.size(); ij++)
             {
-            outputResult = outputResult + "\n" + LoadDataAssociations(strFilePath,Input.get(ij));
+            outputResult = outputResult + "\n" + LoadDataAssociations(strFilePath,Input.get(ij),false);
             }
         }
         catch (Exception ex)
@@ -49,13 +50,24 @@ class NonPersRecommender {
         System.out.println(outputResult);
     }
 
-
-    public static String  LoadDataAssociations(String strFilePath, Integer AssocId) throws FileNotFoundException
+    /**
+     * Data loader
+     * @param strFilePath
+     * @param AssocId
+     * @param Simple
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static String  LoadDataAssociations(String strFilePath, Integer AssocId, Boolean Simple) throws FileNotFoundException
     {
 
         Map<java.lang.Integer, java.lang.Double> UserCompare = new TreeMap<java.lang.Integer, java.lang.Double>();
+        Collection<Integer> advUserCompare = new ArrayList<Integer>();
         Collection<ratingsData> UserRatings = new ArrayList<ratingsData>();
         String output = AssocId.toString() ;
+        String adVoutput = AssocId.toString() ;
+        String result ="";
+        Integer xbarValue = 0;
 
         try {
 
@@ -70,6 +82,7 @@ class NonPersRecommender {
 
                 ratingsData ratingsDataVal = new ratingsData();
 
+                //Comparing MovieId with AssocId
                 if(java.lang.Integer.parseInt(nextLine[1]) == AssocId)
                 {
                     UserCompare.put(java.lang.Integer.parseInt(nextLine[0]), java.lang.Double.parseDouble(nextLine[2]));
@@ -80,6 +93,7 @@ class NonPersRecommender {
                     ratingsDataVal.setMovieId(java.lang.Integer.parseInt(nextLine[1]));
                     ratingsDataVal.setRatingValue(java.lang.Double.parseDouble(nextLine[2]));
                     UserRatings.add(ratingsDataVal);
+
                 }
 
             }
@@ -89,21 +103,33 @@ class NonPersRecommender {
         //Y Values
         MovieRatings = findYvalue(UserRatings,UserCompare);
 
+
         //simple association
         Map<java.lang.Integer, java.lang.Double> MovieAssociations = new TreeMap<java.lang.Integer, java.lang.Double>();
 
-        for (Iterator<java.lang.Integer> mr = MovieRatings.keySet().iterator(); mr.hasNext();)
+        //simple association
+        Map<java.lang.Integer, java.lang.Double> AdvMovieAssociations = new TreeMap<java.lang.Integer, java.lang.Double>();
+
+
+        //Advanced Value
+        Map<java.lang.Integer, java.lang.Integer> advMovieRatings = new TreeMap<java.lang.Integer, java.lang.Integer>();
+
+        // findXBarvalue
+        advMovieRatings = findXBarvalue(UserRatings,UserCompare);
+        xbarValue=  XBarvalue(UserRatings,UserCompare);
+
+            for (Iterator<java.lang.Integer> mr = MovieRatings.keySet().iterator(); mr.hasNext();)
         {
             //(x+y)/y
             java.lang.Integer dmr = mr.next();
-
             java.lang.Double Val = (double) (MovieRatings.get(dmr) + UserCompare.size())/UserCompare.size();
-
-            MovieAssociations.put(dmr,(Val -1))   ;
+            MovieAssociations.put(dmr,(Val -1));
+            Float advValue = (float) ((Val-1)/(((float)(advMovieRatings.get(dmr))/(xbarValue))));
+            AdvMovieAssociations.put(dmr,(double) advValue);
 
         }
 
-        //Sorted
+        //Sorted - Simple
         Map<java.lang.Integer, java.lang.Double> SortedMovieAssociations = new TreeMap<java.lang.Integer, java.lang.Double>();
         SortedMovieAssociations = sortMapByValue(MovieAssociations);
         int jjjj = 0;
@@ -115,22 +141,47 @@ class NonPersRecommender {
 
             output = output + ","+ dsmr + "," + SortedMovieAssociations.get(dsmr).toString().substring(0,5);
 
-            if (jjjj >= 5){break;}
+            if (jjjj >= TOP_5){break;}
 
 
         }
+           //Sorted - Advanced
+        Map<java.lang.Integer, java.lang.Double> SortedAdvMovieAssociations = new TreeMap<java.lang.Integer, java.lang.Double>();
+        SortedAdvMovieAssociations = sortMapByValue(AdvMovieAssociations);
+        int adv = 0;
 
-        //System.out.println(output);
-        //return output;
+        for (Iterator<java.lang.Integer> asmr = SortedAdvMovieAssociations.keySet().iterator(); asmr.hasNext();)
+        {
+            adv = adv +1;
+            java.lang.Integer adsmr = asmr.next();
+
+            //output
+            adVoutput = adVoutput + ","+ adsmr + "," + SortedAdvMovieAssociations.get(adsmr).toString().substring(0,5);
+
+            //top 5
+            if (adv >= 5){break;}
+
         }
-
+            if (Simple = true)
+            {
+                result  = adVoutput;
+            } else { result = output;}
+        }
         catch (Exception e) {
         System.out.println(e.getMessage());
          }
-        return output;
+        return result;
 
   }
 
+
+    /**
+     * Sorting
+     * @param map
+     * @param <Integer>
+     * @param <Double>
+     * @return
+     */
     public static<Integer, Double extends Comparable<Double>> Map<Integer, Double> sortMapByValue(Map<Integer, Double> map)
     {
         List<Map.Entry<Integer, Double>> list = new LinkedList<Map.Entry<Integer, Double>>(
@@ -153,8 +204,12 @@ class NonPersRecommender {
     }
 
 
-
-
+    /**
+     *
+     * @param UserRatings
+     * @param UserCompare
+     * @return
+     */
     public static Map<Integer,Integer> findYvalue(Collection<ratingsData> UserRatings, Map<Integer,Double> UserCompare)
     {
        // System.out.println(UserRatings.size());
@@ -196,6 +251,107 @@ class NonPersRecommender {
         return MovieRatings;
     }
 
+    /**
+     *
+     * @param UserRatings
+     * @param UserCompare
+     * @return
+     */
+    public static Map<Integer,Integer> findXBarvalue(Collection<ratingsData> UserRatings, Map<Integer,Double> UserCompare)
+    {
+        //
+         Integer XbarCount = 0;
+        //Map of MovieID and Rating
+        Map<Integer,Integer> advMovieRatings = new TreeMap<Integer, Integer>();
+        Map<Integer,Integer> XbarArray = new TreeMap<Integer, Integer>();
+
+        try {
+
+            //Iterator<ratingsdata> ii=UserRatings.keySet().iterator();ii.hasNext();
+            for (Iterator<ratingsData> ii=UserRatings.iterator();ii.hasNext();)
+            {
 
 
+                ratingsData rd = ii.next();
+                // System.out.println(ii + " " + rd.getUserId());
+
+                if (!UserCompare.containsKey(rd.getUserId()) )
+                {
+                    if (XbarArray.containsKey(rd.getUserId()))
+                    {
+                        XbarCount = XbarCount + 1;
+                        XbarArray.put(rd.getUserId(),XbarCount);
+                    }
+                    else
+                    {
+                        XbarArray.put(rd.getUserId(),XbarCount);
+                    }
+
+
+                    if( advMovieRatings.containsKey(rd.getMovieId()))
+                    {
+                        advMovieRatings.put(rd.getMovieId(),advMovieRatings.get(rd.getMovieId()) +1)   ;
+                    }
+                    else
+                    {
+                        //First time
+                        advMovieRatings.put(rd.getMovieId(),1);
+                    }
+
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+
+        }
+
+        System.out.println(XbarArray.size());
+        return advMovieRatings;
+    }
+
+
+
+    public static Integer XBarvalue(Collection<ratingsData> UserRatings, Map<Integer,Double> UserCompare)
+    {
+        //
+        Integer XbarCount = 0;
+         Map<Integer,Integer> XbarArray = new TreeMap<Integer, Integer>();
+
+        try {
+
+            //Iterator<ratingsdata> ii=UserRatings.keySet().iterator();ii.hasNext();
+            for (Iterator<ratingsData> ii=UserRatings.iterator();ii.hasNext();)
+            {
+
+
+                ratingsData rd = ii.next();
+                // System.out.println(ii + " " + rd.getUserId());
+
+                if (!UserCompare.containsKey(rd.getUserId()) )
+                {
+                    if (XbarArray.containsKey(rd.getUserId()))
+                    {
+                        XbarCount = XbarCount + 1;
+                        XbarArray.put(rd.getUserId(),XbarCount);
+                    }
+                    else
+                    {
+                        XbarArray.put(rd.getUserId(),XbarCount);
+                    }
+
+
+
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+
+        }
+
+        return XbarArray.size();
+    }
 }
